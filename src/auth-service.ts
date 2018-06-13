80f5b5977b9d6e7fecee81138aa1423ce62331ff
Steve Hobbs <elkdanger@gmail.com>
2018-06-13 22:30:53 +0100

    Implemented AuthService

---
+++ src/auth-service.ts
@@ -0,0 +1,74 @@
+import { WebAuth, Auth0DecodedHash } from "auth0-js";
+import { autoinject } from "aurelia-framework";
+import { Router } from "aurelia-router";
+import { EventEmitter } from "events";
+import env from "./environment";
+
+@autoinject
+export class AuthService {
+  private accessToken: string;
+  private id_token: string;
+  private expires_at: string;
+  
+  authNotifier = new EventEmitter();
+
+  auth0 = new WebAuth({
+    domain: env.auth0.domain,
+    clientID: env.auth0.clientId,
+    redirectUri: "http://localhost:8080/callback",
+    audience: "http://movie-list",
+    responseType: "token id_token",
+    scope: "openid"
+  });
+
+  constructor(private router: Router) {}
+
+  handleAuthentication(): void {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+
+        const savedLocation = JSON.parse(localStorage.getItem('location'))
+        const navigateTarget = savedLocation.url || 'home'
+
+        this.router.navigate(navigateTarget);
+
+        this.authNotifier.emit('authChange', { authenticated: true });
+      } else if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  setSession(authResult: Auth0DecodedHash): void {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    this.accessToken = authResult.accessToken;
+    this.id_token = authResult.idToken;
+    this.expires_at = expiresAt;
+  }
+
+  login(): void {
+    localStorage.setItem('location', JSON.stringify({ url: this.router.currentInstruction.fragment }));
+    this.auth0.authorize();
+  }
+
+  logout(): void {
+    delete this.accessToken;
+    delete this.id_token;
+    delete this.expires_at;
+  
+    this.authNotifier.emit('authChange', { authenticated: false });
+    this.router.navigate('home');
+  }
+
+  isAuthenticated(): boolean {
+    if (!this.accessToken)
+      return false;
+
+    const expiresAt = JSON.parse(this.expires_at);
+    return new Date().getTime() < expiresAt;
+  }
+}
--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,9 @@
 export default {
   debug: true,
   testing: true,
-  apiKey: 'd25d7a6ef37e1f8d1d30d96be59c6a3e'
+  apiKey: 'd25d7a6ef37e1f8d1d30d96be59c6a3e',
+  auth0: {
+    clientId: 'uoT93BFVDssY3ye1PykbdEE3ZUNg8uGG',
+    domain: 'elkdanger.eu.auth0.com'
+  }
 };
