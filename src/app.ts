1d1bd385cd1fce32d4f108d07f7c9f7eeece4484
Steve Hobbs <elkdanger@gmail.com>
2018-06-13 12:43:34 +0100

    Configured the router for popular movies

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-export class App {
+import { PLATFORM } from 'aurelia-pal';
+import { RouterConfiguration, Router } from "aurelia-router";
 
+export class App {
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.options.pushState = true;
+
+    config.map([
+      { route: ['', 'popular'], name: 'popular', title: 'Popular movies', moduleId: PLATFORM.moduleName('pages/popularmovies'), nav: true },
+    ]);  
+  }
 }
